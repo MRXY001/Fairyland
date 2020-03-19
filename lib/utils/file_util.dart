28b90c4600353967a33fcd734ac7575efd964c30
MRXY001 <wxy19980615@gmail.com>
2020-03-19 19:58:49 +0800

    完善FileUtil的文件操作函数（发现自带的库已经精简，还多并不需要做成Util）

--- lib/utils/file_util.dart
+++ lib/utils/file_util.dart
@@ -1,18 +1,53 @@
+import 'dart:io';
+
 class FileUtil {
-	static bool createDir(String name) {
-	
+	static createDir(String path) {
+		var dir = Directory(path);
+		if (dir.existsSync())
+			return ;
+		dir.createSync(recursive: true);
 	}
 	
-	static bool deleteDir(String path) {
-	
+	static deleteDir(String path) {
+		var dir = Directory(path);
+		if (!dir.existsSync())
+			return ;
+		dir.deleteSync();
 	}
 	
-	static bool renameDir(String oldPath, String newName) {
-	
+	static renameDir(String oldPath, String newName, {bool override = false}) {
+		var oldDir = Directory(oldPath);
+		if (!oldDir.existsSync())
+			return ;
+		
+		var directory = new Directory(oldPath);
+		String pDir = directory.parent.path;
+		if (!pDir.endsWith('/'))
+			pDir += '/';
+		
+		var newDir = Directory(pDir + newName);
+		if (newDir.existsSync()) {
+		  if (!override)
+		  	return ;
+		  newDir.deleteSync();
+		}
+		
+		directory.renameSync(pDir + newName);
 	}
 	
-	static bool moveDir(String oldPath, String newPath) {
-	
+	static moveDir(String oldPath, String newPath, {bool override = false}) {
+		var oldDir = Directory(oldPath);
+		if (!oldDir.existsSync())
+			return ;
+		var newDir = Directory(newPath);
+		if (newDir.existsSync()) {
+		  if (!override)
+		  	return ;
+		  newDir.deleteSync();
+		}
+		
+		var directory = new Directory(oldPath);
+		directory.renameSync(newPath);
 	}
 	
 	static bool copyDir(String oldPath, String newPath, {bool override=false}) {
@@ -20,40 +55,143 @@ class FileUtil {
 	}
 	
 	static bool isDirExists(String path) {
-	
+		return Directory(path).existsSync();
 	}
 	
+	// ------------------------------------------------------------
 	
-	static bool createFile(String path) {
-	
+	static createFile(String path) {
+		var file = File(path);
+		if (file.existsSync())
+			return ;
+		file.createSync(recursive: true);
 	}
 	
-	static bool renameFile(String oldPath, String newName) {
-	
+	static renameFile(String oldPath, String newName, {bool override = false}) {
+		var oldFile = File(oldPath);
+		if (!oldFile.existsSync())
+			return ;
+		
+		var file = new File(oldPath);
+		var d = file.parent.path;
+		if (!d.endsWith('/'))
+			d += '/';
+		
+		var newFile = File(d + newName);
+		if (newFile.existsSync()) {
+			if (!override)
+				return ;
+			newFile.deleteSync();
+		}
+		file.renameSync(d + newName);
 	}
 	
-	static bool moveFile(String oldPath, String newPath) {
-	
+	static moveFile(String oldPath, String newPath, {bool override = false}) {
+		var oldFile = File(oldPath);
+		if (!oldFile.existsSync())
+			return ;
+		var newFile = File(newPath);
+		if (newFile.existsSync()) {
+		  if (!override)
+		  	return ;
+		  newFile.deleteSync();
+		}
+		return File(oldPath).renameSync(newPath);
 	}
 	
-	static bool deleteFile(String path) {
-	
+	static deleteFile(String path) {
+		var file = new File(path);
+		if (!file.existsSync())
+			return ;
+		File(path).deleteSync();
 	}
 	
 	static bool isFileExists(String path) {
-	
+		return File(path).existsSync();
 	}
+	
+	// -------------------------------------------------------------
 	
 	/// 保存纯文本文件
 	/// 如果文件不存在，自动创建
-	static bool saveText(String path, String content) {
-	
+	static saveText(String path, String content) {
+		File(path).writeAsStringSync(content);
 	}
 	
 	/// 读取纯文本文件
 	/// 如果文件不存在，返回空文本
 	static String readText(String path) {
-		return "";
+		var file = File(path);
+		if (!file.existsSync())
+			return "";
+		return file.readAsStringSync();
 	}
 	
+	// -------------------------------------------------------------
+
+	List<String> entityDirPaths(String path) {
+		var pDir = Directory(path);
+		List<String> files;
+		if (!pDir.existsSync())
+			return files;
+		List<FileSystemEntity> entityList = pDir.listSync(recursive: false, followLinks: false);
+		for(FileSystemEntity entity in entityList) {
+			if (FileSystemEntity.isFileSync(entity.path))
+				continue;
+			files.add(entity.path);
+		}
+		return files;
+	}
+	
+	List<String> entityDirNames(String path) {
+		var pDir = Directory(path);
+		List<String> files;
+		if (!pDir.existsSync())
+			return files;
+		List<FileSystemEntity> entityList = pDir.listSync(recursive: false, followLinks: false);
+		for(FileSystemEntity entity in entityList) {
+			if (FileSystemEntity.isFileSync(entity.path))
+				continue;
+			var fullPath = entity.path;
+			var xie = fullPath.lastIndexOf('/');
+			if (xie == -1)
+				files.add(fullPath);
+			else
+				files.add(fullPath.substring(xie+1, fullPath.length));
+		}
+		return files;
+	}
+	
+	List<String> entityFilePaths(String path) {
+		var pDir = Directory(path);
+		List<String> files;
+		if (!pDir.existsSync())
+			return files;
+		List<FileSystemEntity> entityList = pDir.listSync(recursive: false, followLinks: false);
+		for(FileSystemEntity entity in entityList) {
+			if (!FileSystemEntity.isFileSync(entity.path))
+				continue;
+			files.add(entity.path);
+		}
+		return files;
+	}
+	
+	List<String> entityFileNames(String path) {
+		var pDir = Directory(path);
+		List<String> files;
+		if (!pDir.existsSync())
+			return files;
+		List<FileSystemEntity> entityList = pDir.listSync(recursive: false, followLinks: false);
+		for(FileSystemEntity entity in entityList) {
+			if (!FileSystemEntity.isFileSync(entity.path))
+				continue;
+			var fullPath = entity.path;
+			var xie = fullPath.lastIndexOf('/');
+			if (xie == -1)
+				files.add(fullPath);
+			else
+				files.add(fullPath.substring(xie+1, fullPath.length));
+		}
+		return files;
+	}
 }
