9dd7841a5788fb88c30969750678984f6abb39f5
MRXY001 <wxy19980615@gmail.com>
2020-03-27 23:31:54 +0800

    添加RollItem

--- lib/directory/chapter_item.dart
+++ lib/directory/chapter_item.dart
@@ -1,8 +1,18 @@
 class ChapterItem {
-	String id;
-	String name;
-	String fullName; // 带有章序的章节名
-	DateTime time;
-	String content;
-	int wordCount;
+  String id;
+  String name;
+  int indexInRoll; // 在当前分卷的索引
+  int indexInBook; // 在全部正文的索引（不包含作品相关）
+  String fullName; // 带有章序的章节名
+  DateTime time;   // 修改时间
+  int wordCount;   // 字数
+  String content;  // 内容
+}
+
+class RollItem {
+  String id;
+  String name;
+  int indexInBook;
+  String fullName;
+  List<ChapterItem> chapters;
 }
